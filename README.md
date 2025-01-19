102a4cfad9449ea3005ead5598cacaf69a00321d
LaylaDesktop <crispylemon7120@gmail.com>
2025-01-19 18:59:29 +0900

    feat(resource): add file upload and Wikipedia search options for quiz resource
    - Enabled users to upload files to use as a quiz resource.

--- README.md
+++ README.md
@@ -5,13 +5,14 @@
 **Nomad Coders - FullstackGPT #9.0 ~ 9.9**
 
 ## ⚒️ Task
-1. [ ] Use Function calling
-2. [ ] Allow the user to customize the difficulty of the test
-3. [ ] make the LLM generate hard or easy question
-4. [ ] Allow the user to retake the test if not all answers are correct
-5. [ ] If all answers are correct use st.ballons
-6. [ ] Allow the user to use its own OpenAI API Key - st.input inside of st.sidebar
-7. [ ] Using st.sidebar put a link to the Github repo with the code of your Streamlit app
+1. [x] Users can upload a file or search Wikipedia as the quiz resource.
+2. [ ] Use Function calling
+3. [ ] Allow the user to customize the difficulty of the test
+4. [ ] make the LLM generate hard or easy question
+5. [ ] Allow the user to retake the test if not all answers are correct
+6. [ ] If all answers are correct use st.balloons
+7. [x] Allow the user to use its own OpenAI API Key - st.input inside st.sidebar
+8. [x] Using st.sidebar put a link to the Github repo with the code of your Streamlit app
 
 ## 📸 Screenshots
 
--- app.py
+++ app.py
@@ -1,12 +1,33 @@
+from pathlib import Path
+
 import streamlit as st
-from langchain.chat_models import openai, ChatOpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.retrievers import WikipediaRetriever
+from langchain.text_splitter import CharacterTextSplitter
+
+Path("./.cache/quiz_files").mkdir(parents=True, exist_ok=True)
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
 
 st.set_page_config(
     page_title="Quiz GPT",
     page_icon="🤔"
 )
-
-
 
 st.markdown(
     """
@@ -22,15 +43,36 @@ with st.sidebar:
     st.title("OpenAI API KEY")
     API_KEY = st.text_input("Use your API KEY")
 
-    file = st.file_uploader(
-        "Upload a .txt, .pdf or .docx file",
-        type=["pdf", "txt", "docx"],
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        )
     )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .txt, .pdf or .docx file",
+            type=["pdf", "txt", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+            st.write(docs)
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=3)
+            with st.status("Searching..."):
+                docs = retriever.get_relevant_documents(topic)
+                st.write(docs)
 
-    st.title("🔗 Github Repo")
-    st.markdown("[![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/Layla7120/Quiz-GPT)")
-
+    st.markdown("""
+    ### 🔗 Github Repo 
+    
+    [![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/Layla7120/Quiz-GPT)
+    """)
     st.subheader("2025-01-19")
+
 
 if API_KEY:
     try:
@@ -47,3 +89,5 @@ if API_KEY:
         st.error(f"Failed to initialize ChatOpenAI: {e}")
 else:
     st.warning("Please enter your OpenAI API key in the sidebar to proceed.")
+
+
